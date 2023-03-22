08bdea65228b9ae37526f107cb2f1511120ca8c7
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-03-23 01:39:17 +0300

    develop four rectangle with animation

--- SwiftUIAnimation/BasicAnimations/BasicAnimationExerciseView.swift
+++ SwiftUIAnimation/BasicAnimations/BasicAnimationExerciseView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct BasicAnimationExerciseView: View {
     @State private var change: Bool = false
     var body: some View {
+        FourRectangleAnimation()
+    }
+    
+    @ViewBuilder
+    func CircleWithAnimation() -> some View {
         VStack {
             GeometryReader { geometry in
                 Circle()
@@ -17,6 +22,7 @@ struct BasicAnimationExerciseView: View {
                     .frame(width: 100, height: 100)
                     // .offset(x: change ? geometry.size.width - 100 : 0, y: change ? geometry.size.height - 100 : 0 )
                     .offset(x: (geometry.size.width - 100) / 2 , y: change ? geometry.size.height - 100: 0)
+                    // .offset(change ? CGSize(width: (geometry.size.width - 100) / 2, height: geometry.size.height - 100) : CGSize(width: (geometry.size.width - 100) / 2, height: 0))
                     .animation(Animation.easeInOut, value: change)
             }
             .padding()
@@ -28,6 +34,49 @@ struct BasicAnimationExerciseView: View {
         }
         .font(.title)
     }
+    
+    @ViewBuilder
+    func FourRectangleAnimation() -> some View {
+        var offsetValue: CGFloat = 200
+        
+        VStack(spacing: 20) {
+            Spacer()
+            VStack(spacing: 1) {
+                HStack(alignment: .bottom, spacing: 1) {
+                    Rectangle()
+                        .frame(width: 70, height: 35)
+                        .offset(x: change ? 0 : -offsetValue)
+                    Rectangle()
+                        .frame(width: 35, height: 70)
+                        .offset(y: change ? 0 : -offsetValue)
+                }.offset(x: -18)
+                HStack(alignment: .top, spacing: 1) {
+                    Rectangle()
+                        .frame(width: 36, height: 72)
+                        .offset(y: change ? 0 : offsetValue)
+                    Rectangle()
+                        .frame(width: 72, height: 36)
+                        .offset(x: change ? 0 : offsetValue)
+                }.offset(x: 18)
+                
+                
+            } //: VSTACK
+            .rotationEffect(.degrees(change ? -90 : 0))
+            .foregroundColor(change ? .red : .orange)
+            .opacity(change ? 1 : 0)
+            .animation(.default, value: change)
+                
+            Spacer()
+            
+            Button("Change") {
+                change.toggle()
+            }.padding(.bottom)
+            
+        } //: VSTACK
+        .font(.title)
+       
+    }
+        
 }
 
 struct BasicAnimationExerciseView_Previews: PreviewProvider {
