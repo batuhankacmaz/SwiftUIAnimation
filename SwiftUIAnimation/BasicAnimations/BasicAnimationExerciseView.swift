03704e5feb1ea66c20d8162a7dee67c4183e27b5
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-03-23 00:58:57 +0300

    move cross direction to circle with animation

---
+++ SwiftUIAnimation/BasicAnimations/BasicAnimationExerciseView.swift
@@ -0,0 +1,36 @@
+//
+//  BasicAnimationExerciseView.swift
+//  SwiftUIAnimation
+//
+//  Created by Batuhan Kacmaz on 23.03.2023.
+//
+
+import SwiftUI
+
+struct BasicAnimationExerciseView: View {
+    @State private var change: Bool = false
+    var body: some View {
+        VStack {
+            GeometryReader { geometry in
+                Circle()
+                    .foregroundColor(.orange)
+                    .frame(width: 100, height: 100)
+                    .offset(x: change ? geometry.size.width - 100 : 0, y: change ? geometry.size.height - 100 : 0 )
+                    .animation(Animation.easeInOut, value: change)
+            }
+            .padding()
+            Spacer()
+            Button("Change") {
+                change.toggle()
+            }
+            .padding(.bottom)
+        }
+        .font(.title)
+    }
+}
+
+struct BasicAnimationExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        BasicAnimationExerciseView()
+    }
+}
