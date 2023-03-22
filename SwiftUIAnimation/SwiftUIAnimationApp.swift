1138e191ab2c84671e5b4ea75a595ffa219f3b16
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-03-23 00:42:10 +0300

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Batuhan Kacmaz on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
